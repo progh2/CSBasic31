41a6a57b2a6bf6a84ffc7159c897e915229d4f3f
Gihun Ham <ham@e-mirim.hs.kr>
2021-04-16 10:21:24 +0900

    #2 기본용어에 대한 실습. 이모지 변수명이 안되는구나!!

--- CSBasic31/Program.cs
+++ CSBasic31/Program.cs
@@ -8,8 +8,23 @@ namespace CSBasic31
 {
     class Program
     {
+        string Var = "테스트";
+
         static void Main(string[] args)
         {
+            var name = "함" + "기" + "훈";
+            Console.Write("개행이 안되는 녀석");
+            Console.WriteLine("개행이 되는 녀석");
+
+            var alpha = "알파";
+            //var 273alpha = "숫자이름";
+            var 한글변수명 = "된당";
+            //string 😀 = "이모지는 안된다";
+
+            // 인라인 주석
+            /*
+             여러줄 주석
+             */
         }
     }
 }
