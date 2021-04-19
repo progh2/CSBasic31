2fa84e7784348fbbf6668ecc356f34f8e100b224
Gihun Ham <ham@e-mirim.hs.kr>
2021-04-19 15:26:57 +0900

    #4 기본 자료형 실습 예제 쉽지만 실수하기 쉬운 것들

--- CSBasic31/Program.cs
+++ CSBasic31/Program.cs
@@ -30,6 +30,73 @@ namespace CSBasic31
             Console.Write("개행이 안되는 출력");
             Console.WriteLine("");
             Console.WriteLine("개행이 되는 출력");
+
+            Console.WriteLine(52);
+            // Arithmetic Operator + - * / 
+            Console.WriteLine(52 + 273); 
+            // Operator Priority
+            Console.WriteLine(5 + 3 * 2); 
+
+            Console.WriteLine(10 / 5);
+            Console.WriteLine(10 % 5); 
+            Console.WriteLine(7 % 3); 
+
+            // Real Number 
+            Console.WriteLine(52.273);
+
+            Console.WriteLine(0);
+            Console.WriteLine(0.0);
+
+            Console.WriteLine(1.0 + 2.0);
+            Console.WriteLine(1.0 - 2.0);
+            Console.WriteLine(1.0 * 2.0);
+
+            Console.WriteLine(1.0 / 2.0);
+            Console.WriteLine(1 / 2);
+            Console.WriteLine(1.0 / 2);
+            Console.WriteLine(1 / 2.0);
+
+            Console.WriteLine('A');
+            Console.WriteLine('가');
+            //Console.WriteLine('AB'); // 문자 Error
+            Console.WriteLine("AB");  // 문자열 ok
+
+            // Escape Character
+            Console.WriteLine("미\t림여자정보\t과학고");
+            Console.WriteLine("미림\t여자정보\t과학고");
+            Console.WriteLine("미림여\t자정보\t과학고");
+            Console.WriteLine("미림여자정보과학고\t안녕");       
+            Console.WriteLine("\\ 역슬래시 \n 엔터문자 \" 큰따옴표");
+
+            // 문자열 연결 연산자 Concatenation Operator 
+            Console.WriteLine("A" + "B");
+            Console.WriteLine("A" + 1);
+            Console.WriteLine(1 + "A");
+            Console.WriteLine("1" + "1");
+            Console.WriteLine('A' + 'B');
+            Console.WriteLine('A' - 'B');
+            //Console.WriteLine("A" - "B");
+            Console.WriteLine("안녕하세요!"[0]);
+            Console.WriteLine("안녕하세요!"[1]);
+            Console.WriteLine("안녕하세요!"[2]);
+            Console.WriteLine("안녕하세요!"[3]);
+            //Console.WriteLine("안녕하세요!"[100]);
+            //Console.WriteLine("안녕하세요!"[-1]);
+            
+            // bool / boolean
+            Console.WriteLine(true);
+            Console.WriteLine(false);
+
+            // Comparison operator 
+            // == != > < >= <=
+            // => =<  <---- no 
+            Console.WriteLine(62 < 273);
+
+            Console.WriteLine(DateTime.Now.Hour
+                < 3 || 8 < DateTime.Now.Hour );
+            Console.WriteLine( 3 < DateTime.Now.Hour
+                && DateTime.Now.Hour < 8);
+
         }
     }
 }
