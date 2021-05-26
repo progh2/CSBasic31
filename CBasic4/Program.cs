cfbb0506fc913f36e093993efcd57f85378a1e76
Gihun Ham <ham@e-mirim.hs.kr>
2021-05-26 10:20:39 +0900

    #24 추상화, 클래스 응용

--- CBasic4/Program.cs
+++ CBasic4/Program.cs
@@ -93,6 +93,26 @@ namespace CBasic4
 
             Console.WriteLine(MyMath.PI);
             MyMath.Hello();
+            /*
+            List<Student> students = new List<Student>();
+            students.Add(new Student() { name = "윤인성", grade = 1 });
+            students.Add(new Student() { name = "연하진", grade = 2 });
+            students.Add(new Student() { name = "윤아린", grade = 3 });
+            students.Add(new Student() { name = "윤명월", grade = 4 });
+            students.Add(new Student() { name = "구지연", grade = 1 });
+            students.Add(new Student() { name = "김연화", grade = 2 });
+            students.Add(new Student() { name = "함기훈", favorityFood="밀크티" });
+            */
+            List<Student> students = new List<Student>() {
+                new Student() { name = "윤인성", grade = 1 },
+                new Student() { name = "연하진", grade = 2 },
+                new Student() { name = "윤아린", grade = 3 },
+                new Student() { name = "윤명월", grade = 4 },
+                new Student() { name = "구지연", grade = 1 },
+                new Student() { name = "김연화", grade = 2 },
+                new Student() { name = "함기훈", favorityFood = "밀크티" }
+        };  
+
         }
     }
 
