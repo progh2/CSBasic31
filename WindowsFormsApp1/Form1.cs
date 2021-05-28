4c40d4837498b9790adc11b50047eec8c4a2d4bd
Gihun Ham <ham@e-mirim.hs.kr>
2021-05-28 10:21:17 +0900

    #25 윈도폼 기본

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -10,10 +10,10 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
-    public partial class Form1 : Form
+    public partial class FormMain : Form
     {
 
-        public Form1()
+        public FormMain()
         {
             InitializeComponent();
         }
