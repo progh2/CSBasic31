b45155ba98f5f8ae7d44af00354fd3414e3a611b
Gihun Ham <ham@e-mirim.hs.kr>
2021-06-14 15:30:19 +0900

    #27 오버로딩 - 매개변수 타입 주의

--- CSBasic5/Program.cs
+++ CSBasic5/Program.cs
@@ -15,25 +15,32 @@ namespace CSBasic5
             Console.WriteLine(ml.Multi(103, 32));
             ml.Print();
             Console.WriteLine(ml.Sum(1, 100));
-            Console.WriteLine(MyMath.Abs(52));
-            Console.WriteLine(MyMath.Abs(273));
+            MyMath.Abs(52);
+            MyMath.Abs(273);
+            MyMath.Abs(52.273f);
+            MyMath.Abs(52.273);
+            MyMath.Abs(30000000000);
         }
 
         class MyMath
         {
-            public static int Abs(int input)
+            
+            public static void Abs(int input)
             {
-                return (input < 0) ? -input : input;
-                /*
-                if(input < 0)
-                {
-                    return -input;
-                }
-                else
-                {
-                    return input;
-                }*/
+                Console.WriteLine( "정수:" + ((input < 0) ? -input : input));
             }
+            
+            public static void Abs(double input)
+            {
+                Console.WriteLine("플롯:" + ((input < 0) ? -input : input));
+            }
+            
+            public static void Abs(long input)
+            {
+                Console.WriteLine("롱:" + ((input < 0) ? -input : input));
+            }
+
+            
         }
 
 
