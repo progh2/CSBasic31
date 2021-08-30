4f71155f71f25a2c4ce3b25418f140597593d266
Gihun Ham <ham@e-mirim.hs.kr>
2021-08-30 15:45:23 +0900

    구조체와 구조체 생성자, 객체와의 차이 예제

--- CSBasic7/Program.cs
+++ CSBasic7/Program.cs
@@ -42,6 +42,37 @@ namespace CSBasic7
     }
     class Program
     {
+        class PointClass
+        {
+            public int x;
+            public int y;
+            public PointClass(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        struct Point
+        {
+            public int x;
+            public int y;
+            public string testA;
+            //public string testB = "init";
+            public Point(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+                this.testA = "";
+            }
+            public Point(int x, int y, string a)
+            {
+                this.x = x;
+                this.y = y;
+                this.testA = a;
+            }
+        }
+
         static void NextPosition(int x, int y, int vx, int vy, 
             out int rx, out int ry)
         {
@@ -51,6 +82,30 @@ namespace CSBasic7
 
         static void Main(string[] args)
         {
+            PointClass pcA = new PointClass(10, 20);
+            PointClass pcB = pcA;
+            pcB.x = 100;
+            pcB.y = 200;
+            Console.WriteLine(pcA.x + " / " + pcA.y);
+            Console.WriteLine(pcB.x + " / " + pcB.y);
+            Console.WriteLine();
+
+            Point pA = new Point(10, 20);
+            Point pB = pA;
+            pB.x = 100;
+            pB.y = 200;
+            Console.WriteLine(pA.x + " / " + pA.y);
+            Console.WriteLine(pB.x + " / " + pB.y);
+            Console.WriteLine();
+
+            Point point = new Point(10, 10);
+            Point point2 = new Point();
+            //point.x = 10;
+            //point.y = 10;
+            Console.WriteLine(point2.x);
+            Console.WriteLine(point2.y);
+
+
             int x = 0;
             int y = 0;
             int vx = 100;
