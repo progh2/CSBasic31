24225970d21fb8802e7ed4cfa73f4125ab00d4ae
Gihun Ham <ham@e-mirim.hs.kr>
2021-08-30 15:10:45 +0900

    out 키워드 예제

--- CSBasic7/Program.cs
+++ CSBasic7/Program.cs
@@ -42,8 +42,36 @@ namespace CSBasic7
     }
     class Program
     {
+        static void NextPosition(int x, int y, int vx, int vy, 
+            out int rx, out int ry)
+        {
+            rx = x + vx;
+            ry = y + vy;
+        }
+
         static void Main(string[] args)
         {
+            int x = 0;
+            int y = 0;
+            int vx = 100;
+            int vy = 100;
+            Console.WriteLine("현재 좌표: (" + x + ", " + y + ")");
+            NextPosition(x, y, vx, vy, out x, out y);
+            Console.WriteLine("다음 좌표: (" + x + ", " + y + ")");
+
+            Console.WriteLine("숫자 입력: ");
+            int output = int.MinValue;
+            bool result = int.TryParse(Console.ReadLine(), out output);
+            if (result)
+            {
+                Console.WriteLine("입력한 숫자:" + output);
+            }
+            else
+            {
+                Console.WriteLine("숫자를 입력해주세요!" + output);
+            }
+
+
             Products p = new Products();
             Console.WriteLine(p[2]);
             Console.WriteLine((new Products())[2]);
