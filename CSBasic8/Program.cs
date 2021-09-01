600e001ed28dc0d69f9ebfc53a995a16e245fb96
Gihun Ham <ham@e-mirim.hs.kr>
2021-09-01 09:53:44 +0900

    파일 입출력 예제

--- CSBasic8/Program.cs
+++ CSBasic8/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -59,6 +60,28 @@ namespace CSBasic8
 
         static void Main(string[] args)
         {
+            File.WriteAllText(@"c:\Users\progh2\Desktop\test.txt", "안녕하세요!");
+            Console.WriteLine(File.ReadAllText(@"c:\Users\progh2\Desktop\test.txt"));
+
+            using (StreamWriter writer = new StreamWriter(@"c:\Users\progh2\Desktop\test2.txt"))
+            {
+                writer.WriteLine("안녕하세요~");
+                writer.WriteLine("StreamWriter 클래스를 사용해");
+                writer.WriteLine("글자를 여러 줄 입력해봅니다.");
+                for(int i = 0; i < 10; i++)
+                {
+                    writer.WriteLine("반복문 - "+i);
+                }
+            }
+            using(StreamReader reader = new StreamReader(@"c:\Users\progh2\Desktop\test2.txt"))
+            {
+                string line;
+                while((line = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }                
+            }
+
             Child child = new Child();
             Parent childAsParent = new Child();
             IDisposable childAsDisposable = new Child();
